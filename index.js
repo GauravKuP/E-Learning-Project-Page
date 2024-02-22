49d7247cc62db7125affeb564d041905bf19192a
GauravKuP <gkgaurav105@gmail.com>
2024-02-22 09:57:20 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+function changeCss(){
+    console.log('hello button');
+    let submitProjectBtn=document.getElementById('submitProjectBtn');
+    submitProjectBtn.getAttribute(id)='NoDisplay';
+    let emailSubmit=document.getElementById('NoDisplay');
+    emailSubmit.id='Display';
+}
+
